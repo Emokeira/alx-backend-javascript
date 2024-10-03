a4ff39f5d5720eaa1aaf051877d590984dfeebf0
Edith <edithmokeira7@gmail.com>
2024-10-03 12:58:49 +0000

    Added: Implemented classRoom class with _maxStudent attribute.

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+//Represents a classroom
+
+export default class ClassRoom {
+// Accepts one parameter, maxStudent size and assigns it to a private field
+  constructor(maxStudentsSize) {
+// The value passed to the constructor is asigned to the private field. 
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
+
